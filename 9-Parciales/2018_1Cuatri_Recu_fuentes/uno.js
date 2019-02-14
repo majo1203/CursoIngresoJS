de02e3940734f159598a9f182f7b0fc60eb8baff
alumno <alumno>
2019-02-14 19:24:31 -0300

    ejercicio 2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var mascota1;
+	var mascota2;
+	var peso1;
+	var peso2;
+	var resultado
+
+	mascota1= prompt("nombre de la mascota 1");
+	mascota2= prompt("nombre de la mascota 2");
+	peso1= prompt("Cual es el peso de mascota 1");
+	peso2= prompt("cual es el peso de mascota 2");
+	resultado= parseInt(peso1) + parseInt(peso2);
+	alert("la suma de los kilos es " + resultado);
 
 }
