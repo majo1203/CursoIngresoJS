9375c1c893872f36786c2922d3f1069625175daf
alumno <alumno>
2019-02-21 21:19:37 -0300

    ejercicios recuperatorio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,54 @@
 function mostrar()
 {
+	var paquete;
+	var formaDePago;
+	var tarjetaVisa;
+	var paypal;
+	var mercadoPago;
+	var efectivo;
+	var otroMedio;
+	var soloDesayunos;
+	var todoIncluido;
+	var precio;
+	var descuento;
 
+	paquete = prompt("Que paquete desea adquirir ");
+	precio = prompt("Cual es el monto de su habitacion");
+	formaDePago = prompt("Cual es su forma de pago");
+	precio = parseInt(precio);
+
+	
+	switch (true) {
+		case "tarjetaVisa":
+		case "mercadoPago":
+		descuento = precio * 10 / 100;
+		break;
+		case "paypal":
+		descuento = precio * 15 / 100;
+		break;
+		case "efectivo":
+		descuento = precio * 20 / 100;
+		break;
+		case "otroMedio":
+		descuento = precio * 5 / 100;
+		break;
+
+	}
+
+	switch (true){
+		case "todoIncluido && paypal":
+		descuento = precio * 25 / 100;
+		break;
+		case "soloDesayunos && efectivo":
+		descuento = precio * 30 / 100;
+		break;
+		case "todoIncluido && efectivo":
+		descuento =  precio * 35 / 100;
+		break;
+
+	}
+
+	total = precio - descuento;
+
+	alert("Su precio con descuento a pagar es " + total);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,54 @@
 function mostrar()
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var precioCuatro;
+	var percioMayor;
+	var mensaje;
+	var descuento;
+	var resultado;
+	var resultadoFinal;
 
+	precioUno = prompt("Ingrese el primer monto");
+	precioDos = prompt("Ingrese el segundo monto");
+	precioTres = prompt("Ingrese el tercer monto");
+	precioCuatro = prompt("Ingrese el cuarto monto");
+
+	var precioUno = parseInt(precioUno);
+	var precioDos = parseInt(precioDos);
+	var precioTres = parseInt(precioTres);
+	var precioCuatro = parseInt(precioCuatro);
+
+	descuento = 100;
+	resultado=precioUno+precioDos+precioTres+precioCuatro;
+
+		if (resultado > 100) {
+		descuento = 90;
+		} else {
+		if (resultado < 50) {
+			descuento = 115;
+	} else {
+		descuento = 95;
+	}
+    }
+
+		resultadoFinal = resultado * descuento / 100;
+
+
+	if (precioUno > precioDos && precioUno > precioCuatro && precioUno > precioTres) 
+	{
+		mensaje = ("El precio mas alto es " + precioUno + " su total es " + resultadoFinal);
+	} else {if (precioDos > precioTres && precioDos > precioCuatro) 
+		{
+		mensaje = ("El precio mas alto es " + precioDos + " su total es " + resultadoFinal);
+	}else{ if (precioTres > precioCuatro) {
+		mensaje = ("El precio mas alto es " + precioTres + " su total es " + resultadoFinal);
+	} else{ if (precioCuatro > precioTres && precioUno > precioDos) {
+		mensaje = ("El precio mas alto es " + precioCuatro + " su total es " + resultadoFinal);
+	}
+} 
+
+
+	alert(mensaje);
 }
